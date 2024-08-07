d8d8afae3bd1091fc52c51c2f27cdafeaff03980
Petr Cibulka <cibulka.me@gmail.com>
2024-08-07 16:01:27 +0200

    fix: adjust build config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import App from "./app";
 
-import App from "./app.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(process.cwd(), "src/main.tsx"),

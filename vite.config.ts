767e7bdb0198e0edeacffd0ec4aee362c3152fc1
Petr Cibulka <cibulka.me@gmail.com>
2024-08-07 15:42:49 +0200

    fix: correct main file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(process.cwd(), "src/main.tsx"),
+      name: "react-tetris-ts",
+      fileName: "main",
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

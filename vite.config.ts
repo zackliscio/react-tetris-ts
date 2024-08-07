6b91e292a974f3bedf784a1bae288b1f887011fc
Petr Cibulka <cibulka.me@gmail.com>
2024-08-07 19:31:39 +0200

    fix: remove all css modification

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
 import path from "path";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin(), dts()],
+  plugins: [react()],
+  base: "./",
   build: {
     lib: {
       entry: path.resolve(process.cwd(), "src/main.tsx"),
@@ -26,42 +23,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
-    },
-  },
-  css: {
-    modules: {
-      generateScopedName: (name, filename) => {
-        if (name === "react-tetris-ts") return "react-tetris-ts.react-tetris-ts";
-        const file = path.basename(filename, ".css").split(".")[0];
-        const result = ("_" + file + "_" + name).replace(".", "-");
-        return result;
-      },
-    },
-    postcss: {
-      plugins: [
-        tailwindcss("./tailwind.config.js"),
-        /*
-        prefixer({
-          prefix: `.react-tetris-ts`,
-          transform(prefix: string, selector: string, prefixedSelector: string, filePath: string) {
-            let result: string;
-
-            if (filePath.endsWith("view.css") || filePath.match(/node_modules/)) {
-              result = selector;
-            } else if (selector.match(/^(html|body)/)) {
-              result = selector.replace(/^([^\s]*)/, `$1 ${prefix}`);
-            } else {
-              result = prefixedSelector;
-            }
-
-            console.log({ prefix, selector, prefixedSelector, filePath, result });
-
-            return result;
-          },
-        }),
-        */
-        autoprefixer({}),
-      ],
     },
   },
 });
